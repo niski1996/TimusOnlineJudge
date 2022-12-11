7916fa51e8ba1941fb3eba763e2c7b0631407540
Karol Ziobro <ziobro.k@e80group.com>
2022-12-11 23:26:45 +0100

    initial commit added 1264

---
+++ 1264/C#_1264_656.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+public class Trial
+{
+    private static void Main()
+    {
+		string[] input = Console.ReadLine().Split(
+		   new char[] { ' ', '\t', '\n', '\r' },
+		   StringSplitOptions.RemoveEmptyEntries);
+
+		Console.WriteLine(Convert.ToInt32(input[0]) * (Convert.ToInt32(input[1])+1));
+
+    }
+}
